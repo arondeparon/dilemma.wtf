f2bf12925e82d73a16a747602a4ee752a707bcb7
Aron Rotteveel <hi@aron.codes>
2024-05-03 16:36:50 +0200

    twitter card

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,11 @@
     <script src="//unpkg.com/alpinejs" defer></script>
     @if (isset($hash))
         <meta property="og:image" content="{{ url('/storage/opengraph/' . $hash) }}.png">
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@arondeparon" />
+        <meta name="twitter:title" content="Dilemma.wtf" />
+        <meta name="twitter:description" content="@yield('title')" />
+        <meta name="twitter:image" content="{{ url('/storage/opengraph/' . $hash) }}.png" />
     @endif
     <title>@yield('title', 'Dilemma.wtf')</title>
     <script defer src="https://analytics.gistreader.com/script.js" data-website-id="7e435c54-925c-49d5-a922-5e10df3fb149"></script>

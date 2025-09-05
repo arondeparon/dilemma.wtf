fcb4599fe3fe757ef99cfe1c7a36853ba898e422
Aron Rotteveel <hi@aron.codes>
2025-09-05 16:00:15 +0200

    exlude zeros

--- app/Http/Controllers/RankingController.php
+++ app/Http/Controllers/RankingController.php
@@ -9,6 +9,10 @@ class RankingController extends Controller
     public function __invoke()
     {
         $dilemmas = Decision::with('firstDilemma', 'secondDilemma')
+            ->where(function ($q) {
+                $q->where('first_dilemma_votes', '>', 0)
+                  ->orWhere('second_dilemma_votes', '>', 0);
+            })
             ->orderByDesc('first_dilemma_votes')
             ->orderByDesc('second_dilemma_votes')
             ->get();
